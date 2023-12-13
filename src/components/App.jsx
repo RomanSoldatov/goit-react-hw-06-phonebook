1ed9a6d6fbdd99d0aadc5c8f5d32adb72566f46a
Roman <roman.soldatov13@gmail.com>
2023-12-13 11:55:39 +0200

    try 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
+import { useSelector } from 'react-redux';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout/Layout';
+import { Section } from './Section/Section';
+import { Header } from './Header/Header';
+import { Filter } from './Filter/Filter';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+
+import { getContacts } from 'redux/contacts/contacts-selectors';
+
 export const App = () => {
+  const contacts = useSelector(getContacts);
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      <Section title="PhoneBook">
+        <ContactForm />
+        {contacts.length > 0 && (
+          <>
+            <Header title="Contacts" />
+            <Filter />
+            <ContactList />
+          </>
+        )}
+      </Section>
+      <ToastContainer />
+      <GlobalStyle />
+    </Layout>
   );
 };
